5a036cc8d4e109a400f4e0e1813b3bc32e54e42e
riyaverma85 <riyaverma2976@gmail.com>
2025-09-03 15:03:12 -0700

    port

--- server/server.js
+++ server/server.js
@@ -3,3 +3,9 @@ const express=require("express");
 const cors=require("cors");
 require("dotenv").config();
 const multer=require("multer");
+
+const Port=process.env.PORT ||4000
+
+app.listen(Port,()=>{
+   console.log(`server running on port ${Port}`);
+})
